740d18d9d6285ff93a2869433dd9d3d061340f11
astsu <you@example.com>
2022-04-06 22:54:38 +0300

    refactor: Update products entities.

--- pizza_store/entities/products.py
+++ pizza_store/entities/products.py
@@ -5,12 +5,29 @@ from decimal import Decimal
 
 @dataclass(frozen=True)
 class Category:
+    """Product category.
+
+    Attributes:
+        id: category id.
+        name: category name (example: "Pizzas").
+    """
+
     id: uuid.UUID
     name: str
 
 
 @dataclass(frozen=True)
 class ProductVariant:
+    """Product variant.
+
+    Attributes:
+        id: product variant id.
+        name: product variant name.
+        weight: weight of this product variant.
+        weight_units: units for `weight`.
+        price: price of this product variant.
+    """
+
     id: uuid.UUID
     name: str
     weight: Decimal
@@ -20,6 +37,15 @@ class ProductVariant:
 
 @dataclass(frozen=True)
 class ProductWithoutVariants:
+    """Product
+
+    Attributes:
+        id: product id.
+        name: product name (example: name of pizza).
+        category: product category.
+        image_url: url to product image.
+    """
+
     id: uuid.UUID
     name: str
     category: Category
@@ -28,9 +54,30 @@ class ProductWithoutVariants:
 
 @dataclass(frozen=True)
 class Product(ProductWithoutVariants):
+    """Product
+
+    Attributes:
+        id: product id.
+        name: product name (example: name of pizza).
+        category: product category.
+        image_url: url to product image.
+        variants: list of product variants.
+    """
+
     variants: list[ProductVariant]
 
 
 @dataclass(frozen=True)
 class ProductVariantWithProduct(ProductVariant):
-    product: Product
+    """Product variant with info about product.
+
+    Attributes:
+        id: product variant id.
+        name: product variant name.
+        weight: weight of this product variant.
+        weight_units: units for `weight`.
+        price: price of this product variant.
+        product: product info.
+    """
+
+    product: ProductWithoutVariants

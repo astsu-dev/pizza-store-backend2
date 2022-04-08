f31016c07048af1e046e47fc5abba640a92da46a
astsu <you@example.com>
2022-04-08 18:55:15 +0300

    Add application settings.

---
+++ pizza_store/settings.py
@@ -0,0 +1,11 @@
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    edgedb_dsn: str
+    jwt_algorithm: str = "HS256"
+    jwt_secret: str
+    jwt_expires_in: int = 24 * 60 * 60  # 1 day
+
+
+settings = Settings(_env_file=".env", _env_file_encoding="utf-8")
